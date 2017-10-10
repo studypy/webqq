5dda911bca7bebaa5a493e11ffb3954ce683ffa5
zero <547644673@qq.com>
2017-10-10 16:34:22 +0800

    flist better

--- static/index/js/chat.js
+++ static/index/js/chat.js
@@ -1,60 +1,60 @@
-var ws = new WebSocket("ws://10.0.142.104:8000/chat")
-var chatuser = []
-function sendmessage() {
-    var msg = document.getElementById("message").value
-    if (msg == "") {
-        return
-    }
-    $("#charhistory").html($("#charhistory").html() + "<p style='text-align: right'>" + msg + "<span style='color: red'>:[我]</span>" + "</p>")
-    document.getElementById("message").value = ""
-    var msguser = "<span style='color:blue;'>"
-    if (chatuser.length > 0)
-    {
-        msguser = "<span style='color:purple;font-weight: bolder'>"
-        msguser += "."
-        for( i in chatuser){
-            msguser += chatuser[i] + "."
-        }
-    }
-    ws.send(msguser + "|" + msg)
-}
-
-function change() {
-    $.getJSON('/flist', function (data) {
-        $("#left").html("")
-        var temp = []
-        for (i in data){
-            for (j in chatuser){
-                if (data[i] == chatuser[j]) {
-                    temp.push(data[i])
-                    break
-                }
-            }
-        }
-        chatuser = temp
-        for (i in data) {
-            var  color = "<p>"
-            for (j in chatuser){
-                if (data[i] == chatuser[j]) {
-                    color = "<p style='color: red'>"
-                }
-            }
-            $("#left").html($("#left").html() + color + data[i] + "</p>")
-        }
-        $("#left p").click(function () {
-            if($(this).css("color").toString() == "rgb(53, 61, 68)"){
-                $(this).css("color","rgb(255, 0, 0)")
-                chatuser.push($(this).text().toString())
-            }
-            else {
-                $(this).css("color","rgb(53, 61, 68)")
-                chatuser.pop($(this).text().toString())
-            }
-        })
-    })
-}
-
 $(document).ready(function () {
+    var ws = new WebSocket("ws://10.0.142.104:8000/chat")
+    var chatuser = []
+
+    function sendmessage() {
+        var msg = document.getElementById("message").value
+        if (msg == "") {
+            return
+        }
+        $("#charhistory").html($("#charhistory").html() + "<p style='text-align: right'>" + msg + "<span style='color: red'>:[我]</span>" + "</p>")
+        document.getElementById("message").value = ""
+        var msguser = "<span style='color:blue;'>"
+        if (chatuser.length > 0) {
+            msguser = "<span style='color:purple;font-weight: bolder'>"
+            msguser += "."
+            for (i in chatuser) {
+                msguser += chatuser[i] + "."
+            }
+        }
+        ws.send(msguser + "|" + msg)
+    }
+
+    function change() {
+        $.getJSON('/flist', function (data) {
+            $("#left").html("")
+            var temp = []
+            for (i in data) {
+                for (j in chatuser) {
+                    if (data[i] == chatuser[j]) {
+                        temp.push(data[i])
+                        break
+                    }
+                }
+            }
+            chatuser = temp
+            for (i in data) {
+                var color = "<p>"
+                for (j in chatuser) {
+                    if (data[i] == chatuser[j]) {
+                        color = "<p style='color: red'>"
+                    }
+                }
+                $("#left").html($("#left").html() + color + data[i] + "</p>")
+            }
+            $("#left p").click(function () {
+                if ($(this).css("color").toString() == "rgb(53, 61, 68)") {
+                    $(this).css("color", "rgb(255, 0, 0)")
+                    chatuser.push($(this).text().toString())
+                }
+                else {
+                    $(this).css("color", "rgb(53, 61, 68)")
+                    chatuser.pop($(this).text().toString())
+                }
+            })
+        })
+    }
+
     ws.onmessage = function (data) {
         $("#charhistory").html($("#charhistory").html() + "<p>" + data.data + "</p>")
         if (data.data.toString().match("^<span style=\'color:((green)|(yellow));")) {
